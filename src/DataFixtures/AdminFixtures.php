2dbfc103fcba4f3fe695f616ac736d54c3491830
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-08-28 22:19:24 +0200

    add Fixtures Admin/Chien (vide)

---
+++ src/DataFixtures/AdminFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AdminFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new Admin();
+        $admin->setEmail('admin@test.fr');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'admin'
+        );
+        $admin->setPassword($hashedPassword);
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setNom('Rakoto');
+        $admin->setPrenom('Mathieu');
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
