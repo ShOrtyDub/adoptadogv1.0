e2d6cb4d41fd4dadb1cab572ec6dd450fad463c2
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-08-29 14:58:39 +0200

    modif formulaire Utilisateur

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -4,6 +4,13 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +19,53 @@ class UtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('age')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Visiteur' => 'ROLE_VISITOR',
+                ],
+                'label' => 'Role',
+                'required' => true
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Mot de passe ne correspond pas',
+                'options' => ['attr' => ['class' => 'password-field', 'autocomplete' => 'new-password']],
+                'required' => is_null($builder->getData()->getId()),
+//                'empty_data' => '',
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe']
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'required' => true
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true
+            ])
+            ->add('age', NumberType::class, [
+                'label' => 'Âge',
+                'required' => true
+            ])
             ->add('photo')
-            ->add('telephone')
-            ->add('is_valide')
-            ->add('fk_id_admin')
-        ;
+            ->add('telephone', NumberType::class, [
+                'label' => 'Téléphone',
+                'required' => true
+            ]);
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    return [$rolesString];
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
