985562fb7a4792d2b82f85559a22c0d8ab3081a0
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-09-01 11:26:33 +0200

    add vue adminMain

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Repository\AdminRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(AdminRepository $adminRepository): Response
     {
-        return $this->render('main/index.html.twig');
+        $utilisateur = $this->getUser();
+        if ($utilisateur instanceof Admin) {
+
+            return $this->render('admin/adminMain.html.twig');
+        } else {
+
+            return $this->render('main/index.html.twig');
+        }
+
     }
 }
